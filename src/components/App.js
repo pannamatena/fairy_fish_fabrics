a6a718d4158cd271fbaf8c917e312283120ba0be
Panna Zsamba <panna@powerednow.com>
2018-12-01 09:25:08 +0000

    Add svgs and footer

--- src/components/App.js
+++ src/components/App.js
@@ -4,42 +4,73 @@ import { Parallax, Background } from 'react-parallax';
 import { animateScroll, scroller } from 'react-scroll';
 import { colours } from '../resources/colours';
 import { fonts } from '../resources/fonts';
+import { facebook, instagram, pinterest } from '../resources/img/icons';
 import Gallery from './Gallery';
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
+
+    this.sidePadding = 20;
+    this.defaultHeaderHeight = 80;
 
     this.state = {
       isScrolled: false,
-      windowInnerHeight: (window.innerHeight - 60),
+      isBottomReached: false,
+      windowInnerHeight: (window.innerHeight - this.sidePadding - this.defaultHeaderHeight),
     };
 
+    this.trackScroll = this.trackScroll.bind(this);
     this.checkIfScreenIsScrolled = this.checkIfScreenIsScrolled.bind(this);
     this.setWindowInnerHeight = this.setWindowInnerHeight.bind(this);
+    this.displayFooter = this.displayFooter.bind(this);
   }
 
-  componentDidMount () {
-    window.addEventListener('scroll', this.checkIfScreenIsScrolled);
+  componentDidMount() {
+    window.addEventListener('scroll', this.trackScroll);
     window.addEventListener('resize', this.setWindowInnerHeight);
   }
 
-  componentWillUnmount () {
-    window.removeEventListener('scroll', this.checkIfScreenIsScrolled);
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.trackScroll);
   }
 
-  checkIfScreenIsScrolled () {
+  checkIfScreenIsScrolled() {
     const scrollTop = window.scrollY;
     this.setState({
       isScrolled: scrollTop !== 0
     });
   }
 
-  setWindowInnerHeight () {
+  setWindowInnerHeight() {
     this.setState({
-      windowInnerHeight: (window.innerHeight - 60),
+      windowInnerHeight: (window.innerHeight - this.sidePadding - this.defaultHeaderHeight),
     });
   }
+
+  displayFooter() {
+    if ((window.scrollY > 50) && !this.state.isBottomReached) {
+      return '60px';
+    }
+    if (this.state.isBottomReached) {
+      return '200px';
+    }
+    return '0';
+  }
+
+  isBottomReached(el) {
+    return el.getBoundingClientRect().bottom <= window.innerHeight;
+  }
+
+  trackScroll() {
+    this.checkIfScreenIsScrolled();
+
+    const wrappedElement = document.getElementById('page_container');
+    this.setState({
+      isBottomReached: this.isBottomReached(wrappedElement),
+    });
+    document.removeEventListener('scroll', this.trackScroll);
+  };
 
   render() {
     const outerContainer = css`
@@ -52,7 +83,7 @@ class App extends Component {
       max-width: 1400px;
     `;
     const innerContainer_2 = css`
-      margin: 0 30px;
+      margin: 0 20px;
     `;
     const styleButton = {
       primary: css`
@@ -83,20 +114,20 @@ class App extends Component {
       `,
       pageContainer: css`
         height: 100%;
-        padding-top: 30px;
+        padding-top: 80px;
       `,
       headerContainer: css`
-        background: ${this.state.isScrolled ? colours.c1 : 'transparent'};
+        background: ${colours.c1};
         border-bottom: ${this.state.isScrolled ? `1px dashed ${colours.c8}` : 'none'};
         z-index: 99;
         position: fixed;
-        top: ${this.state.isScrolled ? '0' : '30px'};
+        top: 0;
         left: 50%;
         transform: translateX(-50%);
         transition: all 0.2s ease;
         
-        padding-top: ${this.state.isScrolled ? '10px' : '30px'};
-        padding-bottom: ${this.state.isScrolled ? '10px' : '30px'};
+        padding-top: ${this.state.isScrolled ? '10px' : '20px'};
+        padding-bottom: ${this.state.isScrolled ? '10px' : '20px'};
       `,
       headerContainer__inner: css`
         display: flex;
@@ -104,12 +135,12 @@ class App extends Component {
         align-items: center;
         justify-content: flex-start;
         
-        margin: 0 60px;
+        margin: 0 20px;
       `,
       logo: css`
         transition: all 0.2s ease;
         font-family: ${fonts.f2};
-        color: ${this.state.isScrolled ? colours.c2 : colours.c1 };
+        color: ${colours.c2};
         
         font-size: 28px;
       `,
@@ -126,8 +157,8 @@ class App extends Component {
         
         a {
           transition: all 0.2s ease;
-          font-family: ${fonts.f2};
-          color: ${this.state.isScrolled ? colours.c2 : colours.c1 };
+          font-family: ${fonts.f1};
+          color: ${colours.c2};
           
           font-size: 28px;
           
@@ -171,11 +202,66 @@ class App extends Component {
           color: ${colours.c1};
         }
       `,
+      footerContainer: css`
+        width: 100%;
+        background: ${colours.c1};
+        z-index: 99;
+        position: fixed;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        transition: all 0.2s ease;
+        border-top: 1px dashed ${colours.c8};
+        height: ${this.displayFooter()};
+      `,
+      footerContainer__inner: css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        
+        margin: 0 20px;
+        padding: 20px 0;
+      `,
+      socialLinks: css`
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-start;
+        
+        li {
+          margin: 0 10px;
+        }
+        
+        svg {
+          width: 30px;
+          height: 30px;
+          fill: ${colours.c2};
+          transition: all 0.2s ease;
+        }
+        
+        a:hover {
+          svg {
+            fill: ${colours.c3};
+          }
+        }
+      `,
+      credentials: css`
+        color: ${colours.c8};
+        
+        a {
+          color: ${colours.c8};
+        }
+        
+        a:hover {
+          color: ${colours.c4};
+        }
+      `,
     };
 
     return (
       <div className={style.app}>
-          <div className={style.pageContainer}>
+          <div id="page_container" className={style.pageContainer}>
               <div className={`${style.headerContainer} ${outerContainer}`}>
                 <div className={`${style.headerContainer__inner}`}>
                   <a className={style.logo} href="/">FairyFish Fabrics</a>
@@ -196,9 +282,6 @@ class App extends Component {
                     renderLayer={percentage => (
                         <div
                             className={style.promotionContainer__text}
-                            style={{
-                              top: `${percentage * 80}%`,
-                            }}
                         >
                           <a href="/">[logo img]</a>
                           <h2>Handwoven shawls and scarves</h2>
@@ -213,7 +296,16 @@ class App extends Component {
                 </div>
               </div>
               <div className="socialContainer">social</div>
-              <div className="footerContainer">footer</div>
+              <div className={style.footerContainer}>
+                <div className={style.footerContainer__inner}>
+                  <ul className={style.socialLinks}>
+                    <li><a href="/">{facebook()}</a></li>
+                    <li><a href="/">{instagram()}</a></li>
+                    <li><a href="/">{pinterest()}</a></li>
+                  </ul>
+                  <div className={style.credentials}>Icons made by <a href="https://www.flaticon.com/authors/graphicsbay" title="GraphicsBay">GraphicsBay</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                </div>
+              </div>
           </div>
       </div>
     );
